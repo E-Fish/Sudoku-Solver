e1e78f25cfa47b01124afec4ab1fc1ed7aef503a
pippapflaum <105876667+pippapflaum@users.noreply.github.com>
2023-11-17 18:21:51 -0500

    Add files via upload

--- SudokuNode.java
+++ SudokuNode.java
@@ -1,54 +1,40 @@
-package code;
-
 import java.util.LinkedList;
 import java.util.List;
 
 public class SudokuNode {
 	SudokuState state;
 	SudokuNode parent;
+    List<Integer> currentMoveOptions;
     List<SudokuNode> children;
-    
-   
+
+    //constructor
     public SudokuNode(SudokuState s, SudokuNode p) {
-        this.state = s;
-        this.parent = p;
-        this.children = new LinkedList<SudokuNode>();
+        state = s;
+        parent = p;
+        List<SudokuNode> c = new LinkedList<SudokuNode>();
+        children = c;
     }
     
     public SudokuState getState() {
-        return this.state;
-    }
-    
+        return this.state;}
     public SudokuNode getParent() {
-    	return this.parent;
-    }
+    	return this.parent;}
+    public List<SudokuNode> getChildren(){
+        return children;}
+
     public SudokuNode clone() {
     	SudokuNode newNode = this;
     	return newNode;
     }
-    
-    public List<SudokuNode> getChildren(){
-		return children;
-	}
-    //creates copy of board for generateChildren() so that board can be modified and returned without altering current
-  /*  private int[][] copyBoard(int[][] ogBoard) {
-    	
-        int[][] copy = new int[ogBoard.length][ogBoard[0].length];
-        
-        for (int i = 0; i < ogBoard.length; i++) {
-            System.arraycopy(ogBoard[i], 0, copy[i], 0, ogBoard[0].length);
-        }
-        return copy;
-    }*/
-   
+
    void generateChildren(){
 	   int[][] b = state.getBoard();
-	 		      List<Integer> moves = state.validAction();
+	 		      List<Integer> moves = state.getPossibleMoves();
 	 		      for (Integer move: moves) {
-	 		        		b[state.currMoveRow(b)][state.currMoveCol(b)]=move;
-	 		        		SudokuState nextS = new SudokuState(b, state.getMoves()+1);
-	 		        		SudokuNode nextN = new SudokuNode(nextS, this);
-	 		        		children.add(nextN);
+	 		        		b[state.currMoveRow(b)][state.currMoveCol(b)]=move;//finds index of first 0
+	 		        		SudokuState nextState = new SudokuState(b, state.getMoves()+1);
+	 		        		SudokuNode nextNode = new SudokuNode(nextState, this);
+	 		        		children.add(nextNode);
 	 		   }
    }
 }
