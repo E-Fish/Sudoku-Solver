762ef5f156bae70eca92b0a195dbec7a172c8f43
stace25 <150371138+stace25@users.noreply.github.com>
2023-11-17 17:51:56 -0500

    Add files via upload

--- SudokuMain.java
+++ SudokuMain.java
@@ -1,11 +1,108 @@
 package code;
 
-import java.util.LinkedList;
-import java.util.List;
+	import java.util.ArrayList;
+	import java.util.Stack;
+	import java.util.List;
+	
 
-class SudokuMain {
+	class SudokuMain {
 
-	/* public boolean solve(SudokuNode starter) {
+	    // STACY METHOD HERE
+	    public static void main (String[]array){
+	        // initial game state
+	          int [][] board = { // [row][column]
+	            {4,0,1,0},
+	            {0,2,0,0},
+	            {0,0,3,0},
+	            {0,0,0,1}
+	            };
+	        //goal state
+	        int [][] goalBoard = {
+	            {4,3,1,2},
+	            {1,2,4,3},
+	            {2,1,3,4},
+	            {3,4,2,1}
+	            };
+	      
+	        //goalBoard.initalize();
+	        
+	        SudokuState initialState = new SudokuState(board,0);
+	        SudokuNode initialNode = new SudokuNode(initialState, null);
+	        List<SudokuNode> frontier = new ArrayList<SudokuNode>();
+	        frontier.add(initialNode);
+	        Stack<SudokuState> goalPath = new Stack<SudokuState>();
+	        List<SudokuState> explored = new ArrayList<SudokuState>();
+
+	        // iterate thru states
+	                    SudokuNode curr= initialNode;
+	                        for(int j=0; j<4; j++){
+	                            for(int k=0; k<4; k++){
+	                                if(curr.getState().getBoard()[j][k]==0){//finds next blank
+	                                   // System.out.println("else");
+	                                  //  List<Integer> newMoves = curr.getState().validAction();//applyvalidaction returns a list, applyihgn this to curr gets new children
+	                                    //PIPPA COME BACK TO
+	                                    //PIPPA WORKING HERE
+	                                    
+	                                 //   curr.getChildren().addAll(newMoves);//children is now the list of new moves from this state
+	                                	curr.generateChildren();
+	                                	List<SudokuNode> childNodes =  curr.getChildren();
+	                                    if (childNodes.isEmpty()){
+	                                        ///BACKTRACK
+	                                    	System.out.println("none");
+	                                        curr = curr.getParent();//will always work as long as board is solvable
+	                                    }
+	                                    else{
+	                                       for(SudokuNode move: childNodes){//if it has options to choose from
+	                                    	   {
+	                                            if(!explored.contains(move.getState())){
+	                                                if(move.getState().isGoal(goalBoard)){
+	                                                    goalPath.add(move.getState());
+	                                                    while(curr.parent != null) {
+	                                                        goalPath.add(curr.getState());
+	                                                        curr = curr.getParent();
+	                                                    }
+	                                                    frontier.clear();//ends while loop
+	                                                    break; // i think this ends for loop?
+	                                                }
+	                                                 else{
+	                                                      explored.add(curr.getState());
+	                                                      curr=move;
+	                                                        }
+	                                            }
+	                                            break;
+	                                        }
+	                                        }
+	                                }    
+	                            }
+	                           while(!goalPath.isEmpty()) {
+	                        	   goalPath.pop().printState();
+	                           }
+	                	     
+	                        }
+	                           
+	                        }
+	                        System.out.println("done");
+	    }
+	}
+	
+
+	                                    
+	                                 //for random generating
+	                                 /*  for(int l=0; l<curr.getChildren().size(); l++){
+	                                        if(curr.getChildren().get(l)==null){
+	                                            //STACY START HERE!!!
+	                                            //go back to repository
+	                                            //trigger stacy code?
+	                                        }
+	                                        
+	                                        */
+	                                      
+	                        
+	        
+	            //generate possible moves, add to children frontier
+	            // randomly choose a move
+	        
+	/* public static boolean solve(SudokuNode starter) {
 		 int [][] goalBoard = {
 		            {4,3,1,2},
 		            {1,2,4,3},
@@ -24,30 +121,18 @@ class SudokuMain {
  		    {
  		        for(int j=0;j<b.length;j++)
  		        {
- 		        	if (b[i][j]==0) {
- 		        	List<Integer> moves = curr.getState().applyValidAction();
- 		        	for (Integer move: moves) {
- 		        		if (curr.getState().isValid(move)) {
- 		        			b[i][j]=move;
- 		        			SudokuState nextS = new SudokuState(b, curr.getState().getMoves()+1);
- 		        			SudokuNode nextN = new SudokuNode(nextS, curr);
- 		        			if (solve(nextN)) {
- 		        				return true;
- 		        			}
- 		        			else {
- 		        				b[i][j]=0;
- 		        			}
- 		        		}
- 		        		}
+ 		        	curr.generateChildren();
+ 		        	List<SudokuNode> next = curr.getChildren();
+ 		        	for (SudokuNode move :next) {
+ 		        		if (move.)
  		        	}
- 		        	
- 		        }
- 		        	return false;
+
  		    }
  		}
  		return true;
  		}
-     }*/
+     }
+ 
     public static void main (String[]args){
  
         // initial game state
@@ -59,34 +144,19 @@ class SudokuMain {
             };
 
         //goal state
-        int [][] goalBoard = {
+       /* int [][] goalBoard = {
             {4,3,1,2},
             {1,2,4,3},
             {2,1,3,4},
             {3,4,2,1}
-            };
-        SudokuState initialState = new SudokuState(board, 0);
+            };*/
+   /*     SudokuState initialState = new SudokuState(board, 0);
         SudokuNode initialNode = new SudokuNode(initialState, null);
         initialState.printState();
         initialNode.generateChildren();
-        for (SudokuNode child: initialNode.getChildren())
-        {
-        	child.getState().printState();
-        	child.generateChildren();
-        	  for (SudokuNode next: child.getChildren())
-              {
-        		  next.getState().printState();
-        		  next.generateChildren();
-        		  for (SudokuNode small: next.getChildren())
-        	        {
-        			  small.getState().printState();
-        			  small.generateChildren();
-        			  for (SudokuNode orange: small.getChildren())
-          	        {
-        				  orange.getState().printState();
-          	        }
-        	        }
-              }
+        if (solve(initialNode)!=true)
+        	{
+        	System.out.println("done");
         	}
         }
     }
