9adc03f096af7df2dfe665aa609a4d9155102108
pippapflaum <105876667+pippapflaum@users.noreply.github.com>
2023-11-17 13:08:03 -0500

    Add files via upload

--- PSudokuMain.java
+++ PSudokuMain.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.LinkedList;
 
 class PSudokuMain {
+
+    // STACY METHOD HERE
     public static void main (String[]array){
         // initial game state
           int [][] board = { // [row][column]
@@ -31,62 +33,68 @@ class PSudokuMain {
         List<SudokuNode> frontier = new ArrayList<SudokuNode>();
         frontier.add(initialNode);
         List<SudokuState> goalPath = new ArrayList<SudokuState>();
+        List<SudokuState> explored = new ArrayList<SudokuNode>();
     
+
+
         // iterate thru states
-        while(frontier.isEmpty()!=true){
-            System.out.println("while");
-            for(int i=0; i<frontier.size(); i++){//for each node in frontier
-                 SudokuNode curr = frontier.get(i);
-                 //iterate through its array, find next blank, come up with moves, chose move
-                    if(Arrays.deepEquals(curr.getState().getBoard(),goalBoard)){
-                        System.out.print("if");
-                        goalPath.add(curr.getState());
-                        while(curr.parent != null){
-                            curr = curr.getParent();
-                            goalPath.add(curr.getState());
-                        }
-                        frontier.clear();//ends while loop
-                        System.out.println(goalPath);
-                        break; // i think this ends for loop?
-                    }
-                    else{ 
+                    SudokuNode curr= initialNode;
                         for(int j=0; j<4; i++){
                             for(int k=0; k<4; k++){
-                                if(curr.getState().getBoard()[j][k]==0){
-                                    System.out.println("else");
+                                if(curr.getState().getBoard()[j][k]==0){//finds next blank
+                                   // System.out.println("else");
                                   //  List<Integer> newMoves = curr.getState().validAction();//applyvalidaction returns a list, applyihgn this to curr gets new children
                                     //PIPPA COME BACK TO
                                     //PIPPA WORKING HERE
-                                    curr.generateChildren();
+                                    
                                  //   curr.getChildren().addAll(newMoves);//children is now the list of new moves from this state
-                                    for(int l=0; l<curr.getChildren().size(); l++){
+                                    List<SudokuNodes> childNodes =  curr.generateChildren();
+                                    if (childNodes.isEmpty()){
+                                        ///BACKTRACK
+                                        curr = curr.getParent();//will always work as long as board is solvable
+                                    }
+                                    else{
+                                        for (int n=0; n<childNodes.size(); n++){//if it has options to choose from
+                                            if(!explored.contains(childNodes.get(n).getState())){
+                                                if(childNode.get(n).getState().isGoal(goalBoard)){
+                                                    goalPath.add(childNode.get(n).getState());
+                                                    while(curr.parent != null){
+                                                        curr = curr.getParent();
+                                                        goalPath.add(curr.getState());
+                                                    }
+                                                    frontier.clear();//ends while loop
+                                                    System.out.println(goalPath);
+                                                    break; // i think this ends for loop?
+                                                }
+                                                 else{
+                                                      explored.add(curr.getState());
+                                                      curr=childNodes.get(n);
+                                                        }
+                                            }
+                                            break;
+                                        }
+                                        }
+                                }    
+                            }
+
+                                    
+                                 //for random generating
+                                 /*  for(int l=0; l<curr.getChildren().size(); l++){
                                         if(curr.getChildren().get(l)==null){
                                             //STACY START HERE!!!
                                             //go back to repository
                                             //trigger stacy code?
                                         }
                                         
-                                        else{
-                                            Random random = new Random();
-                                            int randomIndex = random.nextInt(curr.getChildren().size());
-                                            SudokuNode randomNode = curr.getChildren().get(randomIndex);//children is a list of nodes
-                                          //  int[][] newBoard= board.clone();
-                                          //  newBoard[i][j] = randomNode;
-
-                                          //  SudokuState newState = new SudokuState(newBoard,curr.getState().getMoves()+1);
-                                          //  SudokuNode newNode = new SudokuNode(newState, curr);
-                                            frontier.add(randomNode);
-                                            System.out.println(randomNode.getState());
-                                        }
-                                    }
-                                }
-                            }
+                                        */
+                                      
                         }
-                    }
+                        
+                    
                 
-            }    
-        }
+             
         
+        System.out.println("done");
             //generate possible moves, add to children frontier
             // randomly choose a move
         // 
